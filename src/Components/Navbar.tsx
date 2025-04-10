f51924d5933d9e0f62394aa12e67d9e4d66edb1f
feranmiola <osunjuyigbeiyin@gmail.com>
2025-04-10 20:58:10 +0100

    Update Navbar component to include search functionality and integrate Radix UI dialog for improved user experience; also update package dependencies.

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,10 @@
-import { Phone, PhoneCall, Search } from "lucide-react";
-import React from "react";
+"use client";
+import { PhoneCall, Search, XIcon } from "lucide-react";
+import React, { useState } from "react";
+import { Sheet, SheetContent } from "./ui/sheet";
 
 const Navbar = () => {
+  const [isSearchSheetOpen, setIsSearchSheetOpen] = useState(true);
   return (
     <div className="w-screen flex flex-row p-10 items-center justify-between">
       <div className="flex flex-row space-x-18">
@@ -35,7 +38,7 @@ const Navbar = () => {
             <span className="absolute left-0 bottom-0 h-[1px] bg-white w-0 group-hover:w-full transition-all duration-300 origin-left"></span>
           </p>
 
-          <div className="pl-5">
+          <div onClick={() => setIsSearchSheetOpen(true)} className="pl-5">
             <Search className="cursor-pointer" color="white" />
           </div>
         </div>
@@ -53,6 +56,35 @@ const Navbar = () => {
           Let's talk
         </button>
       </div>
+
+      <Sheet open={isSearchSheetOpen} onOpenChange={setIsSearchSheetOpen}>
+        <SheetContent side="top" className="w-full bg-[#131921] h-[400px]">
+          <div className="flex flex-col w-full h-full items-center space-y-18 p-10">
+            <div className="flex flex-row w-full items-center justify-between">
+              <img
+                src="https://res.cloudinary.com/debiu7z1b/image/upload/v1744309237/logo-inverse2-nocopyright_a9hhgt.webp"
+                alt="logo"
+                className="w-[150px] h-auto"
+              />
+
+              <div onClick={() => setIsSearchSheetOpen(false)}>
+                <XIcon size={40} color="white" className="cursor-pointer" />
+              </div>
+            </div>
+
+            <div className="flex w-[70%] pb-4 border-b border-b-[#20262D]">
+              <div className="flex w-full flex-row items-center justify-between">
+                <input
+                  className="w-[70%] text-[#989CA9] text-2xl outline-none focus:outline-none "
+                  placeholder="Type anything and hit enter"
+                />
+
+                <Search className="cursor-pointer" color="white" size={30} />
+              </div>
+            </div>
+          </div>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
