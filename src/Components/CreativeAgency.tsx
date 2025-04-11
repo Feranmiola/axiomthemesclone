cfc7e8ec9d6e8aa396039276d7e73f9ff44eddee
feranmiola <osunjuyigbeiyin@gmail.com>
2025-04-11 10:12:52 +0100

    Add image support in next.config.ts and enhance CreativeAgency component layout with Cloudinary images for better visual presentation.

--- src/Components/CreativeAgency.tsx
+++ src/Components/CreativeAgency.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import DesignIcon from "./Icons/DesignIcon";
 import UploadIcon from "./Icons/UploadIcon";
+import Image from "next/image";
 
 const CreativeAgency = () => {
   return (
-    <div className="flex flex-row items-center justify-center w-full space-x-10">
+    <div className="flex flex-row items-center justify-center w-full space-x-28 py-10">
+      <div className="relative w-[600px] h-[600px]">
+        <Image
+          src="https://res.cloudinary.com/debiu7z1b/image/upload/v1744362475/h3l_uytasz.webp"
+          alt="h3l"
+          width={412}
+          height={413}
+          className="absolute left-0 top-0 z-0"
+        />
+
+        <Image
+          src="https://res.cloudinary.com/debiu7z1b/image/upload/v1744362474/h3r_ggqhl3.webp"
+          alt="h3r"
+          width={416}
+          height={485}
+          className="absolute right-0 bottom-0 z-10"
+        />
+      </div>
+
       <div className="flex flex-col space-y-8">
         <p className="font-medium">CREATIVE AGENCY</p>
         <p className="text-[64px] font-medium leading-none">
